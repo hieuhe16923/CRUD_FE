c7338a939fcea40194699168d16e3275167e8d5d
vanvuong <truongvuong712219@gmail.com>
2025-08-09 00:25:52 +0700

    khoi tao du an

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-center">
+        Welcome to the CRUD Application with FindById functionality!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
